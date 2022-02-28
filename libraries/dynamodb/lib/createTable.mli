6037ea07c907469be7a33ad9066e047515923348
2or3 <t.nagashima.cog@gmail.com>
2022-03-01 00:47:25 +0900

    Fix dynamodb create table interface

--- libraries/dynamodb/lib/createTable.mli
+++ libraries/dynamodb/lib/createTable.mli
@@ -1,10 +1,10 @@
 Open Types
 
-type input = createTableRequest.t
+type input = CreateTableRequest.t
 
 type output = unit
 
-type error = Errors_internal.type
+type error = Errors_internal.t
 
 include
   Aws.Call with type input := input and type output := output and type error := error
